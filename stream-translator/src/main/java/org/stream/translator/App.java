c1b697294c4c8cdcecde363f18f49636ed1e3a5a
jean.paul.ik@gmail.com <jean.paul.ik@gmail.com@4a60fb7c-e689-11de-91d6-89caad0352f8>
2011-07-14 13:08:11 +0000

    initial project

---
+++ stream-translator/src/main/java/org/stream/translator/App.java
@@ -0,0 +1,13 @@
+package org.stream.translator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
